2be8bc3eea4edce202d20851a30f3e1a26d872c6
kathe2 <kathelyncj02@gmail.com>
2023-09-11 11:58:08 -0300

    Resolvendo os bugs do cep

--- js/clientes.js
+++ js/clientes.js
@@ -1,18 +1,11 @@
-const enderecoForm = document.querySelector(".form-body");
-const cepInput = document.querySelector("#cep");
-const ruaInput = document.querySelector("#rua");
-const bairroInput = document.querySelector("#bairro");
-const cidadeInput = document.querySelector("#cidade");
-const estadoInput = document.querySelector("#estado");
+var enderecoForm = document.querySelector(".form-body");
+var cepInput = document.querySelector("#cep");
+var ruaInput = document.querySelector("#rua");
+var bairroInput = document.querySelector("#bairro");
+var cidadeInput = document.querySelector("#cidade");
+var estadoInput = document.querySelector("#estado");
+var formInput = document.querySelector("[data-input]");
 
-cepInput.addEventListener("keypress", (e) => {
-    const onlyNumbers = /[0-9]/;
-    const tecla = String.fromCharCode(e.keyCode);
-    if (!onlyNumbers.test(tecla)){
-        e.preventDefault();
-        return;
-    }
-});
 
 cepInput.addEventListener("keyup", (e) => {
     const inputValue = e.target.value;
@@ -21,11 +14,19 @@ cepInput.addEventListener("keyup", (e) => {
     }
 });
 
-const getEndereco = async (cep) => {
+var getEndereco = async (cep) => {
     const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
+    console.log(formInput);
+    console.log(data.erro);
+    ruaInput.value = data.logradouro;
+    cidadeInput.value = data.localidade;
+    bairroInput.value = data.bairro;
+    estadoInput.value = data.uf;
+
+
 }
 
 
